d6cd274fd4db768667b0b292d9f0c6a2e03bc8c3
Juan <j.d.delacruz120@gmail.com>
2020-02-13 11:41:18 -0600

    Updates

---
+++ precache-manifest.1105909c5d5afc52eedc319fa521fb11.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e3f28ad9afacbed200c3d5af350b53",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "c89441395abc32db330a",
+    "url": "/myapp/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1256b55a296cab30e045",
+    "url": "/myapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c89441395abc32db330a",
+    "url": "/myapp/static/js/2.c35ee024.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/myapp/static/js/2.c35ee024.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1256b55a296cab30e045",
+    "url": "/myapp/static/js/main.2fed1c26.chunk.js"
+  },
+  {
+    "revision": "edb82abcda217baf13b1",
+    "url": "/myapp/static/js/runtime-main.7e519dc1.js"
+  },
+  {
+    "revision": "e3a433f4e1668df94643067958ca6ee2",
+    "url": "/myapp/static/media/header.e3a433f4.JPG"
+  },
+  {
+    "revision": "576549ba3dc13313a927fda0fd720d12",
+    "url": "/myapp/static/media/main.576549ba.jpg"
+  }
+]);
