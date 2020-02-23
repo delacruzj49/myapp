18fc6fda8eb1c5455560769bfddd1dfad752ab1a
Juan <j.d.delacruz120@gmail.com>
2020-02-23 16:14:48 -0600

    added some background colors and images.added button to route to full post

--- src/components/about.js
+++ src/components/about.js
@@ -1,24 +1,61 @@
 import React , {Component} from 'react';
 import Styled from 'styled-components';
-import {Container,Jumbotron,Row,Col} from 'react-bootstrap';
+import {Container,Jumbotron,Row,Col,Button} from 'react-bootstrap';
 import Mic from '../assets/landing.jpg';
+import Mics from '../assets/church.jpg';
 
+const ContainerMain= Styled(Container)`
+background:#e5e4cc;
+padding: 10px 30px 10px 30px;
 
-
+`
 const Jumbo = Styled(Jumbotron)`
 text-align:center;
 background:url(${Mic});
 height:300px;
 background-size:cover;
 `
+const Jumbotrons = Styled(Jumbotron)`
+background:#bac7a7;
+margin:0;
+padding:10px;
+border-bottom-style: solid;
+border-bottom-color:#698474;
+`
 
 const Title = Styled.h1`
 font-size:25px;
+color:#698474;
 `
 const Body = Styled.p`
 font-size:15px;
+color:white;
 `
+const JumbotronBackground = Styled(Jumbotron)`
+background:url(${Mics});
+background-size:cover;
+height:250px;
+`
+const TitleHeader = Styled.h1`
+font-size: 85px;
+color:#698474;
+padding-top:60px;
 
+@media (max-width :375px){
+    margin-left:110px;
+}
+
+`
+const Buttons = Styled(Button)`
+color:#698474;
+border-color:#698474;
+background-color:white;
+float:right;
+&:hover{
+    background-color:#698474;
+    color:#e5e4cc;
+}
+`
 
 class AboutSection extends Component {
     constructor(props){
@@ -50,26 +87,31 @@ class AboutSection extends Component {
             return<div>Loading..</div>;
         }
         else{
-
             return(
-                <Container >
+                <ContainerMain>
                     <Jumbo>
+                        <TitleHeader>Blog</TitleHeader>
                     </Jumbo>
                          {items.map(items =>(
-                            <Jumbotron>
+                            <Jumbotrons>
                                 <Row>
                                     <Col>
-
+                                        <JumbotronBackground>
+                                            
+                                        </JumbotronBackground>
                                     </Col>
-                                    <Col key={items.id}>
+                                    <Col md='7' key={items.id}>
                                         <Title>{items.title}</Title>
                                         <Body>{items.body}</Body>
+                                        <div>
+                                            <Buttons variant="secondary">Full Post</Buttons>
+                                         </div>
                                     </Col>
 
                                 </Row>
-                            </Jumbotron>
-                        ))};
-                </Container>
+                            </Jumbotrons>
+                        ))}
+                </ContainerMain>
    
         );
             }
