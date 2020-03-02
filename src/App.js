64a1dcd9d74a6296e56e2652bd7cab1178407b89
Juan <j.d.delacruz120@gmail.com>
2020-03-02 12:09:52 -0600

    Deleted Contact page and added Login Page to react-router and pages

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 //Importing Pages
 import Home from './pages/home.js';
 import About from './pages/about.js';
-import Contact from './pages/contact.js';
 import NotFound from './pages/404.js';
+import AdminLogin from './pages/adminLogin.js';
 
 function App() {
   return (
@@ -19,8 +19,8 @@ function App() {
           <Route path="/about">
             <About/>
           </Route>
-          <Route path="/contact">
-            <Contact/>
+          <Route path="/Login">
+            <AdminLogin/>
           </Route>
           <Route path="*">
              <NotFound/>
