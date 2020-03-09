65b768623b6abda3a7efa49220b366f8b7a148b6
Juan <j.d.delacruz120@gmail.com>
2020-03-09 13:07:38 -0500

    Switched to a react class adding handleChange/handleSubmit for user input.

--- src/components/subscribe.js
+++ src/components/subscribe.js
@@ -10,7 +10,6 @@ width:90%;
 float:center;
 margin:0 auto;
 height:300px;
-padding-top:30px;
 background:none;
 `
 
@@ -38,27 +37,49 @@ width: 100%;
 background:none;
 `
 
-const Subscribe = () => (
+class Subscribe extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    
+      handleChange(event) {
+        this.setState({value: event.target.value});
+      }
+    
+      handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+      }
 
-    <Contain fluid>
-        <Jumbo fluid>
-            <Div>
-                <h1> SUBSCRIBE</h1>
-                <p> Subscribe to get the latest update on the next podcast!</p>
-                <Form>
-                    <Form.Group as={Row} controlId="formHorizontalEmail">
-                        <Form.Label column sm={3}></Form.Label>
-                        <Col sm={6}>
-                            <FormControl type="email" placeholder="Email : example@email.com" />
-                        </Col>
-                    </Form.Group>
-                    <Buttons variant="secondary">Sign up</Buttons>
-                </Form>
-            </Div>
-        </Jumbo>
-    </Contain>
+    render(){
+        return(
+            <Contain fluid>
+                <Jumbo fluid>
+                    <Div>
+                        <h1> SUBSCRIBE</h1>
+                        <p> Subscribe to get the latest update on the next podcast!</p>
+                    
+                        <Form onSubmit={this.handleSubmit}>
+                            <Form.Group as={Row} controlId="formBasicEmail" >
+                                <Form.Label column sm={3}></Form.Label>
+                                <Col sm={6}>
+                                <FormControl type="email" value={this.state.value} onChange={this.handleChange} placeholder="Enter email : name@email.com" />
+                                </Col>
+                            </Form.Group>
+                            <Buttons type="submit" value="submit"  >Sign up</Buttons>
+                        </Form>
+                    </Div>
+                </Jumbo>
+            </Contain>
+        );
+    }
+}
 
-);
+
 
 
 export default Subscribe;
