ae9ad04524bc8de30e9b7b001eb3f1e454967bf9
Juan <j.d.delacruz120@gmail.com>
2020-03-22 22:59:45 -0500

    Connected to the api to post data submited by the user to the DB

--- src/components/message.js
+++ src/components/message.js
@@ -36,37 +36,79 @@ width:100px;
 }
 `
 
-const Message = () => (
-    <Contain fluid>
-        <Jumbo fluid>
-            <Div>
-                <h1> LOVE TO HEAR FROM YA!</h1>
-                <Form>
-                    <Form.Group as={Row} controlId="exampleForm.ControlInputName" >
-                        <Form.Label column sm={3}></Form.Label>
-                        <Col sm={6}>
-                            <FormControl placeholder="First name" />
-                        </Col>
-                    </Form.Group>
-                    <Form.Group as={Row}  controlId="exampleForm.ControlInputEmail">
-                        <Form.Label column sm={3}></Form.Label>
-                        <Col sm={6}>
-                            <FormControl type="email" placeholder="name@example.com" />
-                        </Col>
-                    </Form.Group>
-                    <Form.Group as={Row} controlId="exampleForm.ControlTextareaMessage">
-                        <Form.Label column sm={3}></Form.Label>
-                        <Col sm={6}>
-                            <FormControl as="textarea" rows="3"  placeholder="Thought, Questions, Ideas Comments Here"/>
-                        </Col>
-                    </Form.Group>
-                </Form>
-                <Buttons variant="secondary">Send</Buttons>
-            </Div>
-        </Jumbo>
-    </Contain>
+class Message extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            comment: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        const value = event.target.value;
+        this.setState({
+            ...this.state,
+            [event.target.name]:value
+        });
+    }
+
+    handleSubmit(event){
+        alert('Thanks for your input');
+        event.preventDefault();
+        //Add fetch to post to DB using api
+        fetch('http://localhost:4000/questions',{
+            method:'POST',
+            body:JSON.stringify({
+                name:this.state.name,
+                email:this.state.email,
+                comment:this.state.comment
+            }),
+            headers:{
+                "Content-type":"application/json; charset=utf-8"
+            }
+        })
+    }
+    render(){
+        return(
+            <Contain fluid>
+                <Jumbo fluid>
+                    <Div>
+                        <h1> LOVE TO HEAR FROM YA!</h1>
+                        <Form onSubmit={this.handleSubmit}>
+                            <Form.Group as={Row} controlId="ControlInputName" >
+                                <Form.Label column sm={3}></Form.Label>
+                                <Col sm={6}>
+                                    <FormControl  name ="name" value={this.state.name} onChange={this.handleChange} placeholder="First name" />
+                                </Col>
+                            </Form.Group>
+                            <Form.Group as={Row}  controlId="ControlInputEmail">
+                                <Form.Label column sm={3}></Form.Label>
+                                <Col sm={6}>
+                                    <FormControl  name ="email" value={this.state.email}  onChange={this.handleChange} placeholder="name@example.com"  />
+                                </Col>
+                            </Form.Group>
+                            <Form.Group as={Row} controlId="ControlTextareaMessage">
+                                <Form.Label column sm={3}></Form.Label>
+                                <Col sm={6}>
+                                    <FormControl  name ="comment" value={this.state.comment} onChange={this.handleChange}  as="textarea" rows="3" placeholder="Thought, Questions, Ideas Comments Here" />
+                                </Col>
+                            </Form.Group>
+                            <Buttons  type="submit" value="submit" >Send</Buttons>
+                        </Form>
+                        
+                    </Div>
+                </Jumbo>
+            </Contain>
+    
+        );
+    }
+}
 
 
-);
 
 export default Message;
