bf32c8d31f0c5b4971695d3efbaab0ebc99a5378
Juan <j.d.delacruz120@gmail.com>
2020-03-23 23:56:25 -0500

    added the modal-submission component and got rid of the button

--- src/components/subscribe.js
+++ src/components/subscribe.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Styled from 'styled-components';
-import {Jumbotron,Container,Row,Col,Form,Button} from 'react-bootstrap';
+import {Jumbotron,Container,Row,Col,Form} from 'react-bootstrap';
+
+import ModalShow from './modal-submissions.js';
+
 const Contain = Styled(Container)`
 width:90%;
 `
@@ -15,17 +18,7 @@ background:none;
 const Div = Styled.div`
 text-align:center;
 `
-const Buttons = Styled(Button)`
-color:#698474;
-border-color:#698474;
-background-color:white;
-text-align:center;
-width:100px;
-&:hover{
-    background-color:#698474;
-    color:#e5e4cc;
-}
-`
+
 const FormControl = Styled(Form.Control)`
 padding: 12px 20px;
 margin: 8px 0;
@@ -40,7 +33,6 @@ class Subscribe extends React.Component {
     constructor(props) {
         super(props);
         this.state = {value: ''};
-    
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
       }
@@ -50,7 +42,6 @@ class Subscribe extends React.Component {
       }
     
       handleSubmit(event) {
-          alert("Thanks for Subscribing! ")
         event.preventDefault();
         //add to post api
         fetch('http://localhost:4000/subscribers',{
@@ -63,7 +54,8 @@ class Subscribe extends React.Component {
             }
         })
       }
-      
+
+
     render(){
         return(
             
@@ -72,7 +64,6 @@ class Subscribe extends React.Component {
                     <Div>
                         <h1> SUBSCRIBE</h1>
                         <p> Subscribe to get the latest update on the next podcast!</p>
-                    
                         <Form onSubmit={this.handleSubmit}>
                             <Form.Group as={Row} controlId="formBasicEmail" >
                                 <Form.Label column sm={3}></Form.Label>
@@ -80,10 +71,7 @@ class Subscribe extends React.Component {
                                 <FormControl type="email" value={this.state.value} onChange={this.handleChange} placeholder="Enter email : name@email.com" />
                                 </Col>
                             </Form.Group>
-                            <Buttons type="submit"  >
-                            Sign up
-                            </Buttons>
-                            
+                           <ModalShow component="subscribed"/>   
                         </Form>
                     </Div>
                 </Jumbo>
